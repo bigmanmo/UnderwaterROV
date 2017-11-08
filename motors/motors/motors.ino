dc12f8f81dfc04eca9c2b55476e80638f2f40bbb
Mohamed <mohamed@onion.io>
2017-11-07 20:14:57 -0500

    consolidated sensor code and motor code. need to add a toggle for automatic vs manual control

--- motors/motors/motors.ino
+++ motors/motors/motors.ino
@@ -2,27 +2,40 @@
 #include <Servo.h>
 #include <PS2X_lib.h>
 #include <TaskScheduler.h>
+#include <MadgwickAHRS.h>
+#include <Wire.h>
+#include <SparkFun_MS5803_I2C.h>
+#include "quaternionFilters.h"
+#include "MPU9250.h"
 
 #define PS2_DAT        6  //yellowwhite    
 #define PS2_CMD        2  //orange, blue ground, whiteblue power, 
 #define PS2_SEL        3  //whitegreen
 #define PS2_CLK        4  //green
 
-//#define pressures   true
 #define pressures   false
-//#define rumble      true
 #define rumble      false
 
 #define forward_spin 95
 #define backward_spin 80
+#define SerialDebug false  // Set to true to get Serial output for debugging
 
 volatile PS2X ps2x;
 
+MS5803 sensor(ADDRESS_HIGH);
+
+MPU9250 myIMU;
+Madgwick filter;
 // This is our motor.
 volatile Servo motorR;
 volatile Servo motorL;
 volatile Servo motorB;
 byte vibrate = 0;
+
+//Create variables to store results
+float temperature_c, temperature_f;
+double pressure_abs, pressure_relative, altitude_delta, pressure_baseline;
+
 
 volatile int initial_x = 0;
 volatile int initial_y = 0;
@@ -38,11 +51,15 @@ volatile int z_current = 0;
 void readGamepadCallback();
 void updateMotorBCallback();
 void updateMotorRLCallback();
+void updateImuCallback();
+void updatePressureSensorCallback()
 
 //Tasks
 Task readGamepad(50, TASK_FOREVER, &readGamepadCallback);
 Task updateMotorB(125, TASK_FOREVER, &updateMotorBCallback);
 Task updateMotorLR(125, TASK_FOREVER, &updateMotorRLCallback);
+Task updateImuTask(250, TASK_FOREVER, &updateImuCallback);
+Task updatePressureSensor(250, TASK_FOREVER, &updatePressureSensorCallback);
 
 Scheduler runner;
 
@@ -116,11 +133,168 @@ void updateMotorRLCallback() {
    }
 }
 
+void updateImuCallback() {
+  // If intPin goes high, all data registers have new data
+  // On interrupt, check if data ready interrupt
+  if (myIMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
+  {
+    myIMU.readAccelData(myIMU.accelCount);  // Read the x/y/z adc values
+
+    // Now we'll calculate the accleration value into actual g's
+    // This depends on scale being set
+    myIMU.ax = (float)myIMU.accelCount[0] * myIMU.aRes; // - myIMU.accelBias[0];
+    myIMU.ay = (float)myIMU.accelCount[1] * myIMU.aRes; // - myIMU.accelBias[1];
+    myIMU.az = (float)myIMU.accelCount[2] * myIMU.aRes; // - myIMU.accelBias[2];
+
+    myIMU.readGyroData(myIMU.gyroCount);  // Read the x/y/z adc values
+
+    // Calculate the gyro value into actual degrees per second
+    // This depends on scale being set
+    myIMU.gx = (float)myIMU.gyroCount[0] * myIMU.gRes;
+    myIMU.gy = (float)myIMU.gyroCount[1] * myIMU.gRes;
+    myIMU.gz = (float)myIMU.gyroCount[2] * myIMU.gRes;
+
+    myIMU.readMagData(myIMU.magCount);  // Read the x/y/z adc values
+
+    // Calculate the magnetometer values in milliGauss
+    // Include factory calibration per data sheet and user environmental
+    // corrections
+    // Get actual magnetometer value, this depends on scale being set
+    myIMU.mx = (float)myIMU.magCount[0] * myIMU.mRes
+               * myIMU.factoryMagCalibration[0] - myIMU.magBias[0];
+    myIMU.my = (float)myIMU.magCount[1] * myIMU.mRes
+               * myIMU.factoryMagCalibration[1] - myIMU.magBias[1];
+    myIMU.mz = (float)myIMU.magCount[2] * myIMU.mRes
+               * myIMU.factoryMagCalibration[2] - myIMU.magBias[2];
+  } // if (readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
+
+  // Must be called before updating quaternions!
+  myIMU.updateTime();
+
+  MahonyQuaternionUpdate(myIMU.ax, myIMU.ay, myIMU.az, myIMU.gx * DEG_TO_RAD,
+                         myIMU.gy * DEG_TO_RAD, myIMU.gz * DEG_TO_RAD, myIMU.my,
+                         myIMU.mx, myIMU.mz, myIMU.deltat);
+  filter.updateIMU(myIMU.gx, myIMU.gy, myIMU.gz, myIMU.ax, myIMU.ay, myIMU.az);
+
+ 
+  else
+  {
+    // Serial print and/or display at 0.5 s rate independent of data rates
+    myIMU.delt_t = millis() - myIMU.count;
+
+    // update LCD once per half-second independent of read rate
+    if (myIMU.delt_t > 500)
+    {
+      if(SerialDebug)
+      {
+
+      myIMU.yaw   = atan2(2.0f * (*(getQ()+1) * *(getQ()+2) + *getQ()
+                    * *(getQ()+3)), *getQ() * *getQ() + *(getQ()+1)
+                    * *(getQ()+1) - *(getQ()+2) * *(getQ()+2) - *(getQ()+3)
+                    * *(getQ()+3));
+      myIMU.pitch = -asin(2.0f * (*(getQ()+1) * *(getQ()+3) - *getQ()
+                    * *(getQ()+2)));
+      myIMU.roll  = atan2(2.0f * (*getQ() * *(getQ()+1) + *(getQ()+2)
+                    * *(getQ()+3)), *getQ() * *getQ() - *(getQ()+1)
+                    * *(getQ()+1) - *(getQ()+2) * *(getQ()+2) + *(getQ()+3)
+                    * *(getQ()+3));
+      myIMU.pitch *= RAD_TO_DEG;
+      myIMU.yaw   *= RAD_TO_DEG;
+
+
+      myIMU.yaw  -= 9.62;
+      myIMU.roll *= RAD_TO_DEG;
+
+      if(SerialDebug)
+      {
+        Serial.print("Yaw, Pitch, Roll: ");
+        Serial.print(myIMU.yaw, 2);
+        Serial.print(", ");
+        Serial.print(myIMU.pitch, 2);
+        Serial.print(", ");
+        Serial.println(myIMU.roll, 2);
+
+        Serial.print("rate = ");
+        Serial.print((float)myIMU.sumCount / myIMU.sum, 2);
+        Serial.println(" Hz");
+      }
+
+
+      myIMU.count = millis();
+      myIMU.sumCount = 0;
+      myIMU.sum = 0;
+    }
+  }
+//  if((myIMU.yaw) > 5)
+//  {
+//     motorL.write(100);
+//     motorR.write(90);
+//  }
+//  else if((myIMU.yaw) < -5)
+//  {
+//     motorR.write(100);
+//     motorL.write(90);
+//  }
+//  else
+//  {
+//    motorR.write(90);
+//    motorL.write(90);
+//  }
+}
+}
+
+void updatePressureSensor() {
+  // To measure to higher degrees of precision use the following sensor settings:
+  // ADC_256 
+  // ADC_512 
+  // ADC_1024
+  // ADC_2048
+  // ADC_4096
+    
+  // Read temperature from the sensor in deg C. This operation takes about 
+  temperature_c = sensor.getTemperature(CELSIUS, ADC_512);
+  
+  // Read temperature from the sensor in deg F. Converting
+  // to Fahrenheit is not internal to the sensor.
+  // Additional math is done to convert a Celsius reading.
+  temperature_f = sensor.getTemperature(FAHRENHEIT, ADC_512);
+  
+  // Read pressure from the sensor in mbar.
+  pressure_abs = sensor.getPressure(ADC_4096);
+  
+  // Let's do something interesting with our data.
+  
+  // Convert abs pressure with the help of altitude into relative pressure
+  // This is used in Weather stations.
+  pressure_relative = sealevel(pressure_abs, base_altitude);
+  
+  // Taking our baseline pressure at the beginning we can find an approximate
+  // change in altitude based on the differences in pressure.   
+  altitude_delta = altitude(pressure_abs , pressure_baseline);
+  
+  // Report values via UART
+  Serial.print("Temperature C = ");
+  Serial.println(temperature_c);
+  
+  Serial.print("Temperature F = ");
+  Serial.println(temperature_f);
+  
+  Serial.print("Pressure abs (mbar)= ");
+  Serial.println(pressure_abs);
+   
+  Serial.print("Pressure relative (mbar)= ");
+  Serial.println(pressure_relative); 
+  
+  Serial.print("Altitude change (m) = ");
+  Serial.println(altitude_delta); 
+}
+
 
 // Set everything up
 void setup()
 {
-  // Put the motor to Arduino pin #9
+  // MOTOR Setup *************************************************************************************************************************************
+  // Put the motor to Arduino pins 8,9,10
   motorL.attach(8);
   motorR.attach(9);
   motorB.attach(10);
@@ -155,10 +329,142 @@ void setup()
   runner.addTask(readGamepad);
   runner.addTask(updateMotorB);
   runner.addTask(updateMotorLR);
+  runner.addTask(updateImuTask);
+  runner.addTask(updatePressureSensor);
 
   readGamepad.enable();
   updateMotorB.enable();
   updateMotorLR.enable();
+  // MOTOR Setup **************************************************************************************************************************************
+
+  // IMU Setup ****************************************************************************************************************************************
+  Wire.begin();
+  filter.begin(50);
+// Read the WHO_AM_I register, this is a good test of communication
+  byte c = myIMU.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250);
+  Serial.print(F("MPU9250 I AM 0x"));
+  Serial.print(c, HEX);
+  Serial.print(F(" I should be 0x"));
+  Serial.println(0x71, HEX);
+  if (c == 0x71) // WHO_AM_I should always be 0x71
+  {
+    Serial.println(F("MPU9250 is online..."));
+
+    // Start by performing self test and reporting values
+    myIMU.MPU9250SelfTest(myIMU.selfTest);
+    Serial.print(F("x-axis self test: acceleration trim within : "));
+    Serial.print(myIMU.selfTest[0],1); Serial.println("% of factory value");
+    Serial.print(F("y-axis self test: acceleration trim within : "));
+    Serial.print(myIMU.selfTest[1],1); Serial.println("% of factory value");
+    Serial.print(F("z-axis self test: acceleration trim within : "));
+    Serial.print(myIMU.selfTest[2],1); Serial.println("% of factory value");
+    Serial.print(F("x-axis self test: gyration trim within : "));
+    Serial.print(myIMU.selfTest[3],1); Serial.println("% of factory value");
+    Serial.print(F("y-axis self test: gyration trim within : "));
+    Serial.print(myIMU.selfTest[4],1); Serial.println("% of factory value");
+    Serial.print(F("z-axis self test: gyration trim within : "));
+    Serial.print(myIMU.selfTest[5],1); Serial.println("% of factory value");
+
+    // Calibrate gyro and accelerometers, load biases in bias registers
+    myIMU.calibrateMPU9250(myIMU.gyroBias, myIMU.accelBias);
+
+    myIMU.initMPU9250();
+    // Initialize device for active mode read of acclerometer, gyroscope, and
+    // temperature
+    Serial.println("MPU9250 initialized for active data mode....");
+
+    // Read the WHO_AM_I register of the magnetometer, this is a good test of
+    // communication
+    byte d = myIMU.readByte(AK8963_ADDRESS, WHO_AM_I_AK8963);
+    Serial.print("AK8963 ");
+    Serial.print("I AM 0x");
+    Serial.print(d, HEX);
+    Serial.print(" I should be 0x");
+    Serial.println(0x48, HEX);
+
+    if (d != 0x48)
+    {
+      // Communication failed, stop here
+      Serial.println(F("Communication failed, abort!"));
+      Serial.flush();
+      abort();
+    }
+
+    // Get magnetometer calibration from AK8963 ROM
+    myIMU.initAK8963(myIMU.factoryMagCalibration);
+    // Initialize device for active mode read of magnetometer
+    Serial.println("AK8963 initialized for active data mode....");
+
+    if (SerialDebug)
+    {
+      //  Serial.println("Calibration values: ");
+      Serial.print("X-Axis factory sensitivity adjustment value ");
+      Serial.println(myIMU.factoryMagCalibration[0], 2);
+      Serial.print("Y-Axis factory sensitivity adjustment value ");
+      Serial.println(myIMU.factoryMagCalibration[1], 2);
+      Serial.print("Z-Axis factory sensitivity adjustment value ");
+      Serial.println(myIMU.factoryMagCalibration[2], 2);
+    }
+
+    // Get sensor resolutions, only need to do this once
+    myIMU.getAres();
+    myIMU.getGres();
+    myIMU.getMres();
+
+    // The next call delays for 4 seconds, and then records about 15 seconds of
+    // data to calculate bias and scale.
+//    myIMU.magCalMPU9250(myIMU.magBias, myIMU.magScale);
+    myIMU.magBias[0] = 344.11;
+    myIMU.magBias[1] = 203.31;
+    myIMU.magBias[2] = 117.70;
+    myIMU.magScale[0] = 0.95;
+    myIMU.magScale[1] = 1.03;
+    myIMU.magScale[2] = 1.19;
+    Serial.println("AK8963 mag biases (mG)");
+    Serial.println(myIMU.magBias[0]);
+    Serial.println(myIMU.magBias[1]);
+    Serial.println(myIMU.magBias[2]);
+
+    Serial.println("AK8963 mag scale (mG)");
+    Serial.println(myIMU.magScale[0]);
+    Serial.println(myIMU.magScale[1]);
+    Serial.println(myIMU.magScale[2]);
+    delay(2000); // Add delay to see results before serial spew of data
+
+    if(SerialDebug)
+    {
+      Serial.println("Magnetometer:");
+      Serial.print("X-Axis sensitivity adjustment value ");
+      Serial.println(myIMU.factoryMagCalibration[0], 2);
+      Serial.print("Y-Axis sensitivity adjustment value ");
+      Serial.println(myIMU.factoryMagCalibration[1], 2);
+      Serial.print("Z-Axis sensitivity adjustment value ");
+      Serial.println(myIMU.factoryMagCalibration[2], 2);
+    }
+
+
+  } // if (c == 0x71)
+  else
+  {
+    Serial.print("Could not connect to MPU9250: 0x");
+    Serial.println(c, HEX);
+
+    // Communication failed, stop here
+    Serial.println(F("Communication failed, abort!"));
+    Serial.flush();
+    abort();
+  }
+  // IMU Setup *****************************************************************************************************************************************
+
+  // Pressure Sensor Setup *****************************************************************************************************************************
+
+      //Retrieve calibration constants for conversion math.
+    sensor.reset();
+    sensor.begin();
+    
+    pressure_baseline = sensor.getPressure(ADC_4096);
+  
+  // Pressure Sensor Setup *****************************************************************************************************************************
 }
 
 
@@ -177,4 +483,23 @@ void loop()
   
    //delay(1000);
 }
+
+
+
+ double sealevel(double P, double A)
+// Given a pressure P (mbar) taken at a specific altitude (meters),
+// return the equivalent pressure (mbar) at sea level.
+// This produces pressure readings that can be used for weather measurements.
+{
+  return(P/pow(1-(A/44330.0),5.255));
+}
+
+
+double altitude(double P, double P0)
+// Given a pressure measurement P (mbar) and the pressure at a baseline P0 (mbar),
+// return altitude (meters) above baseline.
+{
+  return(44330.0*(1-pow(P/P0,1/5.255)));
+}
+  
 
